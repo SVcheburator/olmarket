7d5b16b1efe640c165d0820fdee30f501d65e7b8
Cheburator <sashadanilenko06@gmail.com>
2024-06-23 22:24:53 +0200

    apps created

--- olmarket/base/apps.py
+++ olmarket/base/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OlmappConfig(AppConfig):
+class BaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'olmapp'
+    name = 'base'

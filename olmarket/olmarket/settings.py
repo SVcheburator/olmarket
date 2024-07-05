1197ae0ea5c9175239e6771d4ac204cdb274ebb3
Cheburator <sashadanilenko06@gmail.com>
2024-07-05 14:26:13 +0200

    profiles navigation changed, small round avatar added

--- olmarket/olmarket/settings.py
+++ olmarket/olmarket/settings.py
@@ -141,6 +141,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'base', 'static'),
+    os.path.join(BASE_DIR, 'users', 'static'),
 ]
 
 # Default primary key field type

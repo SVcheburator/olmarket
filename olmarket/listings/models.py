bdb6944c4bc89994831d3d19b943bd7e04bd7286
Cheburator <sashadanilenko06@gmail.com>
2024-07-09 17:58:10 +0200

    categories(managed by admin only) added

--- olmarket/listings/models.py
+++ olmarket/listings/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(null=False)
+    icon = CloudinaryField('image', null=True, blank=True)
+
+    def _str_(self):
+        return f"{self.name}"

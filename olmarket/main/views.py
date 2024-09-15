8e038c403ccf2b7eebe1078d709c75e1b0a35401
Cheburator <sashadanilenko06@gmail.com>
2024-09-15 21:52:53 +0200

    listings sorted by categories (not all done)

--- olmarket/main/views.py
+++ olmarket/main/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 
-from listings.models import Listing
+from listings.models import Listing, Category
 
 # Create your views here.
 def main(request):
-    listings = Listing.objects.all()
-
-    for listing in listings:
+    all_listings = Listing.objects.all()
+    for listing in all_listings:
         listing.main_image = listing.images.all()[0].image
 
-    return render(request, 'main/main.html', {'listings': listings})
+    categories = Category.objects.all()
+    category_listings = {}
+    for category in categories:
+        listings_in_category = Listing.objects.filter(category=category)
+        category_listings[category] = listings_in_category
+
+    return render(request, 'main/main.html', {'all_listings': all_listings,'category_listings': category_listings})
